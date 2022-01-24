cd8a621ce9c1c897062255bba7d60916094f3cee
Amisha <msg.amisha@gmail.com>
2022-01-24 23:13:48 +0530

    Added 3 boxes using flex

--- src/App.js
+++ src/App.js
@@ -1,36 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import styled from 'styled-components';
+import TableComponent from './Components/TableComponent';
 
 
-const StyledButton = styled.button`
-display: inline-block;
-  border-radius: 3px;
-  padding: 0.5rem 0;
-  margin: 0.5rem 1rem;
-  width: 11rem;
-  background: transparent;
-  color: white;
-  border: 2px solid white;
-`
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <StyledButton>Click Me</StyledButton>
-      </header>
+      <TableComponent/>
     </div>
   );
 }
---
+++ src/Components/TableComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './flexbox.css'
+function TableComponent() {
+  return <div className='container'>
+      <div className='item'>1</div>
+      <div className='item'>2</div>
+      <div className='item'>3</div>
+  </div>;
+}
+
+export default TableComponent;
