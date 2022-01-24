02ec2f949c441103b79c49caa07bf16aaebd43ae
Amisha <msg.amisha@gmail.com>
2022-01-24 22:26:34 +0530

    Added styled component button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
 
+
+const StyledButton = styled.button`
+display: inline-block;
+  border-radius: 3px;
+  padding: 0.5rem 0;
+  margin: 0.5rem 1rem;
+  width: 11rem;
+  background: transparent;
+  color: white;
+  border: 2px solid white;
+`
 function App() {
   return (
     <div className="App">
@@ -17,6 +29,7 @@ function App() {
         >
           Learn React
         </a>
+        <StyledButton>Click Me</StyledButton>
       </header>
     </div>
   );
