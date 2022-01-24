c813d73eb502e61b1446033a0b5e2e8bd9235934
Amisha <msg.amisha@gmail.com>
2022-01-24 23:40:39 +0530

    Created table using static data

--- src/Components/TableComponent.js
+++ src/Components/TableComponent.js
@@ -2,9 +2,18 @@ import React from 'react';
 import './flexbox.css'
 function TableComponent() {
   return <div className='container'>
-      <div className='item'>1</div>
-      <div className='item'>2</div>
-      <div className='item'>3</div>
+      <div className='item container2'>
+      <div className='item2'>A</div>
+      <div className='item2'>B</div>
+      </div>
+      <div className='item container2'>
+      <div className='item2'>C</div>
+      <div className='item2'>D</div>
+      </div>
+      <div className='item container2'>
+      <div className='item2'>E</div>
+      <div className='item2'>F</div>
+      </div>
   </div>;
 }
 
