0339df089d2cb08024f1b77000ce16622c775b40
Amisha <msg.amisha@gmail.com>
2022-01-25 22:24:50 +0530

    Setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen} from '@testing-library/react';
 import App from './App';
 
 describe("When everything is fine", () => {
   test("should render App Component", () => {
     render(<App/>);
+    screen.debug();
   });
 });
