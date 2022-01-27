6494c559db2f6917572042a47cccf502be6d5a46
Amisha <msg.amisha@gmail.com>
2022-01-27 12:26:47 +0530

    [Amisha | Added a test with queryByRole]

--- src/App.test.js
+++ src/App.test.js
@@ -4,30 +4,32 @@ import App from './App';
 import '@testing-library/jest-dom'
 
 describe("When everything is fine", () => {
+
+  beforeEach(() =>{
+    render(<App/>)
+  })
   test("should render App Component", () => {
-    render(<App/>);
   });
 
   test("should select the children that is being passed to Label Tag", () => {
-    render(<App/>);
     const a = screen.getByText("Text :")
     expect(a).toBeInTheDocument();
   });
 
   test("should select the input element by its role", () => {
-    render(<App/>)
     const a = screen.getByRole("textbox")
     expect(a).toBeInTheDocument()
-  })
+  });
 
   test("should select a label by its text",() => {
-    render(<App/>)
     screen.getByLabelText("Text :")
-  })
+  });
 
   test("should select an input element by its placeholder text",() => {
-    render(<App/>)
     screen.getByPlaceholderText("text")
-  })
+  });
 
+  test("should give null for whatever", () => {
+    expect(screen.queryByRole("whatever")).toBeNull();
+  })
 });
